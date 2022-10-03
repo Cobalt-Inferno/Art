07290451df9a4b6c2f070f627ba20bf273502bbd
art <art>
2022-10-02 23:27:45 -0400

    Added base art

---
+++ art.sh
@@ -0,0 +1,84 @@
+repodir="/tmp/repodir"
+mkdir "$repodir"
+browser="firefox"
+terminal="kitty"
+
+aurpkgs=(
+    vim-plug
+    yay
+    picom-pijulius-git
+)
+pacmanpkgs=(
+    feh
+    emacs
+    vim
+    neovim
+    "$browser"
+    "$terminal"
+    doas
+    networkmanager
+    imlib2
+    xorg-xinit
+    xorg-xinit
+    base-devel
+    bluez
+    bluez-utils
+    blueman
+    pulseaudio
+    pulseaudio-bluetooth
+    pavucontrol
+)    
+
+
+
+cleartmpdir() {
+    if [ -d "$repodir" ]
+    then
+        rm -rf "$repodir"
+    fi
+}
+
+manualinstall() {
+    git clone "https://aur.archlinux.org/$1.git" "$repodir/$1"
+    if [ -d "$repodir/$1" ]
+    then
+        cd "$repodir/$1"
+        makepkg --noconfirm -si >/dev/null || return 1
+    else
+        echo "Error cloning $1 to directory $repodir/$1"
+    fi
+}
+
+makeinstall() {
+    git clone "https://github.com/$1/$2" "$repodir/$1/$2"
+    cd "$repodir/$1/$2"
+    make clean install
+}
+
+installdotfile() {
+    mkdir "$3"
+    git clone "https://github.com/$1/$2" "$repodir/$1/$2"
+    cd "$repodir/$1/$2"
+    mv * "$3"
+}
+
+pacman -S git --noconfirm
+
+for i in "${aurpkgs[@]}"
+do
+    manualinstall "$i"
+done
+for i in "${pacmanpkgs[@]}"
+do
+    pacman -S "$i" --noconfirm
+done 
+# install my dotfiles
+
+makeinstall kavulox dwm
+makeinstall kavulox dwmblocks
+installdotfile kavulox emacs ~/.emacs.d
+installdotfile kavulox nvim ~/.config/nvim
+installdotfile kavulox picom ~/.config/picom
+
+
+cleartmpdir
